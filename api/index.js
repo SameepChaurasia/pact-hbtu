a2e31ea8bb3103cdbcda2111670254e0446982f8
Sameep <chaurasiasameep26@gmail.com>
2025-01-04 20:44:24 +0530

    created signup api route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import userRoutes from './routes/user.route.js';
+import authRoutes from './routes/auth.route.js';
 
 mongoose.connect
     (
@@ -14,8 +15,11 @@ mongoose.connect
 
 const app = express();
 
+app.use(express.json());
+
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
 })
 
 app.use('/api/user', userRoutes);
+app.use('/api/auth', authRoutes);
